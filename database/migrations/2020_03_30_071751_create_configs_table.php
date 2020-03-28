0ab2aa5b4d9af111fa1f796f1b4c4c28047b9212
panxibei <panxibei@yandex.com>
2020-03-28 16:20:37 +0800

    xizhi-iview-admin

--- database/migrations/2020_03_30_071751_create_configs_table.php
+++ database/migrations/2020_03_30_071751_create_configs_table.php
@@ -15,7 +15,7 @@ class CreateConfigsTable extends Migration
     {
         Schema::create('configs', function (Blueprint $table) {
             $table->increments('cfg_id');
-			$table->string('cfg_name',100)->unique()->comment('配置项键名');
+			$table->string('cfg_name', 100)->unique()->comment('配置项键名');
 			$table->text('cfg_value')->nullable()->comment('配置项键值');
 			$table->string('cfg_description')->nullable()->comment('配置项描述');
             $table->timestamps();
